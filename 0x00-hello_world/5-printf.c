b9ce9a47b8bd59ce47506b8c129b54aece5c23a4
Aondongu <syncaondo.group@gmail.com>
2023-02-16 04:15:51 -0800

    scare him

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+/*
+ * File: 5-printf.c
+ * Auth: Jennifer Tang
+ * Date: February 16, 2023
+ * Desc: Prints a message using printf
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the message "with proper grammar, but the outcome is a piece of art,"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0); }
