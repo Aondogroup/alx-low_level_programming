5920efd8fdd6b63c969b5d34e1c2b5a47f5dc41d
Aondongu <syncaondo.group@gmail.com>
2023-02-16 04:24:35 -0800

    constipation

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,11 @@
-/*
- * File: 5-printf.c
- * Auth: Jennifer Tang
- * Date: February 16, 2023
- * Desc: Prints a message using printf
- */
-
-#include <stdio.h>
-
 /**
- * main - Prints the message "with proper grammar, but the outcome is a piece of art,"
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+#include <stdio.h>
+
 int main(void)
-{printf("with proper grammar, but the outcome is a piece of art,\n");
+{ printf("with proper grammar, but the outcome is a piece of art,\n");
+
 return (0); }
