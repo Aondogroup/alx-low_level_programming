65991bceaa6c6df6ee2e38a4865ef90f306770ea
Aondongu <syncaondo.group@gmail.com>
2023-02-17 16:12:47 -0800

    harder

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,34 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - prints all possible combinations of single-digit numbers
  *
- * Description: Prints all possible combinations of single-digit numbers
- *
- * Return: Always 0 (Success)
+ * Return: 0 on success
  */
 int main(void)
 {
-	int num1, num2;
+    int num = 48;
 
-	for (num1 = 0; num1 < 9; num1++)
-	{
-		for (num2 = num1 + 1; num2 <= 9; num2++)
-		{
-			putchar(num1 + '0');
-			putchar(',');
-			putchar(' ');
-			putchar(num2 + '0');
+    while (num <= 57)
+    {
+        putchar(num++);
+        if (num <= 57)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+    }
+    putchar('\n');
 
-			if (num1 != 8 || num2 != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
-
-	putchar('\n');
-
-	return (0);
+    return (0);
 }
