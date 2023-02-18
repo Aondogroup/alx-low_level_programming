14787d8bdd3d47748a3eb0d7016a27fa74b82188
Aondongu <syncaondo.group@gmail.com>
2023-02-18 12:23:35 -0800

    breaks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,13 @@
 /**
  * main - Entry point of the program
  *
+ * Description: This program prints the message "Programming is like
+ * building a multilingual puzzle" to the standard output, followed by a
+ * newline character. It returns a value of 0 to indicate success.
+ *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
-return 0; }
+puts("Programming is like building a multilingual puzzle");
+return (0); }
