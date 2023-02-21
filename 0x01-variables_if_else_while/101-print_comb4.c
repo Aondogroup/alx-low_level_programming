c21e9822668513e551139e8cbdc2e360a2d1d604
Aondongu <syncaondo.group@gmail.com>
2023-02-20 19:58:34 -0800

    pudha p

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,44 +1,32 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints all possible combinations of three digits
+ *        separated by commas, without repeating any digit.
  *
- * Description: Prints all possible different combinations of three digits.
- *              The three digits must be different and in ascending order.
- *              Numbers must be separated by ", ", followed by a space.
- *              No trailing comma at the end.
- *              012, 120, 102, 021, 201, 210 are considered the same combination.
- *              Print only the smallest combination of three digits.
- *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-int i, j, k, l;
+int digit1, digit2, digit3;
 
-for (i = 0; i <= 7; i++)
+for (digit1 = 0; digit1 < 8; digit1++)
 {
-for (j = i + 1; j <= 8; j++)
+for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 {
-for (k = j + 1; k <= 9; k++)
+for (digit3 = digit2 + 1; digit3 <= 9; digit3++)
 {
-for (l = k + 1; l <= 9; l++)
+putchar(digit1 + '0');
+putchar(digit2 + '0');
+putchar(digit3 + '0');
+if (digit1 < 7 || digit2 < 8 || digit3 < 9)
 {
-putchar(i + '0');
-putchar(j + '0');
-putchar(k + '0');
-putchar(l + '0');
-
-if (i == 6 && j == 7 && k == 8 && l == 9)
-break;
-
 putchar(',');
 putchar(' ');
 }
 }
 }
 }
-
 putchar('\n');
 
 return (0); }
