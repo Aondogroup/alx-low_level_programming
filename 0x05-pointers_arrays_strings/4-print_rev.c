b14f21f6192ae31204009e52a2e1cf671b793e2e
Aondongu <syncaondo.group@gmail.com>
2023-02-28 07:17:48 -0800

    moving in circles

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,15 +1,22 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_rev - prints a string in reverse
- * @s: the string to print
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: string to be printed in reverse
+ *
+ * Return: void
  */
 void print_rev(char *s)
 {
-int len = _strlen(s);
+int i, len;
 
-for (int i = len - 1; i >= 0; i--)
-_putchar(s[i]);
+len = 0;
+while (*(s + len) != '\0')
+len++;
+
+for (i = len - 1; i >= 0; i--)
+_putchar(*(s + i));
 
 _putchar('\n');
 }
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,17 +2,19 @@
 
 /**
  * puts2 - prints every other character of a string
- * @str: pointer to the string
+ * @str: pointer to the string to print
  *
  * Return: void
  */
 void puts2(char *str)
 {
-int i;
+int i = 0;
 
-for (i = 0; str[i] != '\0'; i += 2)
+while (str[i] != '\0')
 {
+if (i % 2 == 0)
 _putchar(str[i]);
+i++;
 }
 _putchar('\n');
 }
