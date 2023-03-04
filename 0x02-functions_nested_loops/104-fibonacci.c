a3e6f8d2c385e3ecfce6ea78a96c6f79b4e90ae8
Aondongu <syncaondo.group@gmail.com>
2023-03-04 10:57:58 -0800

    fibonacci sequence

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,19 +7,19 @@
  */
 int main(void)
 {
-    unsigned long int a = 1, b = 2, c;
-    int i;
+unsigned long int a = 1, b = 2, c;
+int i;
 
-    printf("%lu, %lu", a, b);
+printf("%lu, %lu", a, b);
 
-    for (i = 3; i <= 98; i++)
-    {
-        c = a + b;
-        printf(", %lu", c);
-        a = b;
-        b = c;
-    }
+for (i = 3; i <= 98; i++)
+{
+c = a + b;
+printf(", %lu", c);
+a = b;
+b = c;
+}
 
-    printf("\n");
-    return (0);
+printf("\n");
+return (0);
 }
