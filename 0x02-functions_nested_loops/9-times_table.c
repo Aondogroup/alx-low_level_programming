46142d4c5f77f32fd93e59c6e4627a83d85e9570
Aondongu <syncaondo.group@gmail.com>
2023-03-04 04:40:54 -0800

    new lwague of corrections

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,32 +7,32 @@
  */
 void times_table(void)
 {
-    int row, column, product;
+int row, column, product;
 
-    for (row = 0; row <= 9; row++)
-    {
-        for (column = 0; column <= 9; column++)
-        {
-            product = row * column;
-            if (column == 0)
-            {
-                _putchar('0');
-            }
-            else if (product <= 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-                _putchar(' ');
-                _putchar(product + '0');
-            }
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
-                _putchar((product / 10) + '0');
-                _putchar((product % 10) + '0');
-            }
-        }
-        _putchar('\n');
-    }
+for (row = 0; row <= 9; row++)
+{
+for (column = 0; column <= 9; column++)
+{
+product = row * column;
+if (column == 0)
+{
+_putchar('0');
+}
+else if (product <= 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(product + '0');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar((product / 10) + '0');
+_putchar((product % 10) + '0');
+}
+}
+_putchar('\n');
+}
 }
