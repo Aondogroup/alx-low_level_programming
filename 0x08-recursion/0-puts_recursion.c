35dd9322b90a25c8552b06fe9ab372f13f980af2
cieryol <cieryolcontrol@gmail.com>
2023-08-02 11:10:05 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,15 @@
 #include "main.h"
+/**
+ * _puts_recursion -print a string followed by a new line , using recursion
+ * 
+ * @s: Pointer to the string to be printed.
+ */
 
 /*custom implementation of _puts_recursion */
-void _puts_recursion(char *s) {
-if (*s == '\0') {
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
 _putchar('\n');
 return;
 }
