a1aa83022c06cdefc85cd207ccbb2536a1c5e018
Semaka Aondongu Asaaga <180705013@live.unilag.edu.ng>
2023-08-29 21:33:43 +0100

    alx test run

---
+++ lists.h
@@ -0,0 +1,20 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/* Libraries */
+#include <stddef.h>
+
+/* Structures */
+typedef struct listint_s
+{
+	    int n;
+	        struct listint_s *next;
+} listint_t;
+
+/* Function Prototypes */
+int _putchar(char c);
+size_t print_listint(const listint_t *h);
+/* Declare other function prototypes here */
+
+#endif /* LISTS_H */
+
