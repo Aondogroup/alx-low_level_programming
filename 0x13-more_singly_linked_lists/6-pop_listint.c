26d84667668cafad79081251dd18413fb5f84c53
Semaka Aondongu Asaaga <180705013@live.unilag.edu.ng>
2023-08-30 15:14:25 +0100

    new changes

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -4,7 +4,7 @@
 /**
  *pop_listint - Deletes the head node of a listint_t linked list.
  *@head: A pointer to a pointer to the head node.
- * 
+ *
  *Return: The data (n) of the deleted head node, or 0 if the list is empty.
  */
 int pop_listint(listint_t **head)
