c0a322d51c4d4dc66a8a5297cd5f737091a02ff3
Semaka Aondongu Asaaga <180705013@live.unilag.edu.ng>
2023-08-30 15:10:50 +0100

    new changes

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,9 +2,9 @@
 #include "lists.h"
 
 /**
- *  * free_listint2 - Frees a listint_t list and sets the head to NULL.
- *   * @head: Pointer to a pointer to the head of the list.
- *    */
+ * free_listint2 - Frees a listint_t list and sets the head to NULL.
+ * @head: Pointer to a pointer to the head of the list.
+ */
 void free_listint2(listint_t **head)
 {
 listint_t *current, *next;
