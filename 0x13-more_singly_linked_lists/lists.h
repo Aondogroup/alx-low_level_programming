80f2dafd07bcedfbed2274d39c73e12eca8539a4
Semaka Aondongu Asaaga <180705013@live.unilag.edu.ng>
2023-08-30 14:57:02 +0100

    new changes

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -6,16 +6,16 @@
 #include <stdlib.h>
 
 /**
- *  * struct listint_s - singly linked list
- *   * @n: integer
- *    * @next: points to the next node
- *     *
- *      * Description: singly linked list node structure
- *       */
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 typedef struct listint_s
 {
-	    int n;
-	        struct listint_s *next;
+int n;
+struct listint_s *next;
 } listint_t;
 
 /* Function Prototypes */
